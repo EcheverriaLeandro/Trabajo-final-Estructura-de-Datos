4c2597e6a050080063a27ca1f368cfac7f42a7d8
EcheverriaLeandro <ypacifico712@hotmail.com>
2024-11-23 21:10:45 -0300

    se implemento el modulo eliminar al arbol avl

--- arbol.py
+++ arbol.py
@@ -85,6 +85,52 @@ class ArbolAVLPacientes:
         if nodo is None or nodo.izquierdo is None:
             return nodo
         return self.__buscar_minimo(nodo)
+    
+    def __eliminar(self, raiz, id_paciente):
+        if not raiz:
+            return raiz
+        
+        if id_paciente < raiz.id:
+            raiz.izquierdo = self.__eliminar(raiz.derecho, id_paciente)
+        elif id_paciente > raiz.id:
+            raiz.derecho = self.__eliminar(raiz.izquierdo, id_paciente)
+        else:
+            if not raiz.izquierdo:
+                auxiliar = raiz.derecho
+                raiz = None
+                return auxiliar
+            elif not raiz.derecho:
+                auxiliar = raiz.izquierdo
+                raiz = None
+                return auxiliar
+            
+            auxiliar = self.__buscar_minimo(raiz.derecho)
+            raiz.paciente = auxiliar.paciente
+            raiz.derecho = self.__eliminar(raiz.derecha, auxiliar.id)
+            
+        if not raiz:
+            return raiz
+        
+        raiz.altura = max(self.__obtener_altura(raiz.izquierdo), self.__obtener_altura(raiz.derecho))
+        
+        balance = self.__obtener_balance(raiz)
+        
+        #rotacion a la derecha
+        if balance > 1 and self.__obtener_balance(raiz.derecho) >= 0:
+            return self.__rotar_derecha(raiz)
+        #rotacion a la izquierda
+        if balance < -1 and self.__obtener_balance(raiz.izquierdo) <= 0:
+            return self.__rotar_izquierda(raiz)
+        #rotacion izquierda derecha
+        if balance > 1 and self.__obtener_balance(raiz.izquierdo) < 0:
+            raiz.izquierdo = self.__rotar_izquierda(raiz.izquierdo)
+            return self.__rotar_derecha(raiz)
+        #rotacion derecha izquierda
+        if balance < -1 and self.__obtener_balance(raiz.izquierdo) > 0:
+            raiz.derecho = self.__rotar_derecha(raiz.derecho)
+            return self.__rotar_izquierda(raiz)
+        
+        return raiz
         
     def buscar (self, id_paciente): 
         return self.__buscar_paciente(self.raiz, id_paciente)
